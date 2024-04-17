7f9f454c7aa72375f664056556e0b1cb9cb3c1de
simone-nolberto <simone.nolberto@outlook.com>
2024-04-17 15:21:08 +0200

    initial commit (basic visualisation of discs)

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$string = file_get_contents("dischi.json",);
+
+$discsArray = json_decode($string, true);
+// var_dump($discsArray);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booltify</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php foreach ($discsArray as $disc) : ?>
+
+        <ul>
+            <li><?= $disc["title"] ?></li>
+            <li><?= $disc["author"] ?></li>
+            <li><?= $disc["year"] ?></li>
+            <li><?= $disc["genre"] ?></li>
+        </ul>
+
+
+    <?php endforeach; ?>
+
+
+
+
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
